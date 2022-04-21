5938f8bb91ebfc750cea5ffd01ca06a62bc195f4
Mohamed Roshdy <62522577+PASHAMESER@users.noreply.github.com>
2022-04-22 01:53:32 +0200

    Update mian.js

--- js/mian.js
+++ js/mian.js
@@ -83,12 +83,13 @@ imgBoxChat.onclick = function () {
   // fullchat.classList.toggle("ssssss");
   fullchat.style.display = "block";
   imgBoxChat.style.display = "none";
+  textarea.focus();
 }
 
 var MiniMaze = document.getElementById("MiniMaze");
 MiniMaze.onclick = function () {
   fullchat.style.display = "none";
-  imgBoxChat.style.display = "block";
+  imgBoxChat.style.display = "block";  
 }
 
 var imgSend = document.getElementById("img-send");
@@ -97,20 +98,23 @@ var pLeft = document.getElementById("left");
 var pRight = document.getElementById("right");
 
 imgSend.onclick = function () {
-  if(textarea.value == "how are you"){
+  if(textarea.value == "how are you" || textarea.value == "hey"){
     pLeft.innerHTML = textarea.value;
     pLeft.style.padding = "5px";
     textarea.value = "";
   }
   setTimeout(function(){
 
-    pRight.innerHTML ="I'm fine. I hope you are fine. Thank you for contacting us";    
+    pRight.innerHTML ="Hello, I hope you are fine and thank you for contacting us. Can I help you?";
     pRight.style.padding = "5px";
   },1000);
-  
+
 }
 
 /* End chat */
+
+
+
 let date = document.getElementById("date");
 let date1 = new Date();
 let theYear = date1.getFullYear();
